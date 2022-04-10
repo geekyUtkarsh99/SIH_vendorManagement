b94d43472a6454bdac9e962b8a9b8007476c1a3a
geekyUtkarsh99 <utkarshchoudhary80@gmail.com>
2022-04-10 20:09:46 +0530

    admin login operations started

--- vendorBackend/MongoOperations/mdbHandler.py
+++ vendorBackend/MongoOperations/mdbHandler.py
@@ -1,4 +1,5 @@
 from pymongo import MongoClient
+from werkzeug.security import check_password_hash,generate_password_hash
 
 client = MongoClient('mongodb+srv://sihadmin:sih123@sih.2oqaj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
 
@@ -6,11 +7,14 @@ client = MongoClient('mongodb+srv://sihadmin:sih123@sih.2oqaj.mongodb.net/myFirs
 def verify_admin_login(uname, pwd):
     database = client.get_database(name="users")
     doc = database.get_collection(name="vendorBackend_admin")
-    result = doc.find({"username": {"$eq": uname}, "password": pwd})
+    result = doc.find({"username": {"$eq": uname}})
     data = None
     for i in result:
         data = i
-    print(data)
+    # if data is not None:
+    #     if check_password_hash(data['password'],pwd):
+    #         return True
+    #     else : return False
     return data is not None
 
 
