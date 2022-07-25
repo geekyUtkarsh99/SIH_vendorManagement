77601360e97671d44ee75082259478cc47b46144
geekyUtkarsh99 <utkarshchoudhary80@gmail.com>
2022-07-25 14:36:30 +0530

    mongo engine model update

--- server/vendorBackend/serializers.py
+++ server/vendorBackend/serializers.py
@@ -1,8 +1,9 @@
 from rest_framework import serializers
 from .models import CustomerModel, TestModel, VendorModel, admin, CustomercomplainModel
+from rest_framework_mongoengine import serializers
 
 
-class TestSerializer(serializers.ModelSerializer):
+class TestSerializer(serializers.DocumentSerializer):
     class Meta:
         model = TestModel
         fields = ('id',
@@ -10,28 +11,28 @@ class TestSerializer(serializers.ModelSerializer):
                   'val2')
 
 
-class VendorSerializer(serializers.ModelSerializer):
+class VendorSerializer(serializers.DocumentSerializer):
     class Meta:
         model = VendorModel
         fields = ('username', 'password', 'phone', 'email')
 
 
 # for admin---------------
-class adminSerializer(serializers.ModelSerializer):
+class adminSerializer(serializers.DocumentSerializer):
     class Meta:
         model = admin
-        fields = ('admin_id', 'username', 'password','city','state')
+        # fields = ('admin_id', 'username', 'password','city','state')
 
 
 # for Customer...................
 
-class customerSerializer(serializers.ModelSerializer):
+class customerSerializer(serializers.DocumentSerializer):
     class Meta:
         model = CustomerModel
         fields = ('name', 'phone', 'description', 'sanitation', 'service')
 
 
-class customercomplainSerializer(serializers.ModelSerializer):
+class customercomplainSerializer(serializers.DocumentSerializer):
     class Meta:
         model = CustomercomplainModel
         fields = ('name', 'phone', 'ven_id', 'description')
--- server/vendorBackend/urls.py
+++ server/vendorBackend/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from vendorBackend.vendor_views import auth
+from .vendor_views import auth
 # from rest_framework_simplejwt import views as jwt_views
 from . import CustomerView
 
