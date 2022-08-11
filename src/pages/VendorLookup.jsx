6b28e7be840fcda1f93e2022038784e35287627f
Pratik Tayal <pratiktayal786@gmail.com>
2022-08-11 15:38:15 +0530

    reduce files and updated checkbox of filter

--- src/pages/VendorLookup.jsx
+++ src/pages/VendorLookup.jsx
@@ -1,9 +1,142 @@
 import React from 'react'
 import { Form} from 'react-bootstrap';
 import Cards from '../partials/Cards'
-import FilterButton from '../Components/FilterButton';
+import Button from "react-bootstrap/Button";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Popover from "react-bootstrap/Popover";
+import './VendorLookup.css'
 
 export default function VendorLookup() {
+
+    //fiterButton Code 
+
+    function FilterButton() {
+        function SortByItems() {
+          return (
+            <div className="container">
+              <div className="row">
+        
+                <div className="border border-secondary rounded-3 col-sm m-2">
+                  <Form>
+                    <div key="checkbox" className="mb-3">
+                      <Form.Check
+                        type="checkbox"
+                        id="defalt-checkbox"
+                        label="Recently joined"
+                      />
+                    </div>
+                  </Form>
+                  <Form>
+                    <div key="checkbox" className="mb-3 ">
+                      <Form.Check
+                        type="checkbox"
+                        id="defalt-checkbox"
+                        label="Recently joined"
+                      />
+                    </div>
+                  </Form>
+                  <Form>
+                    <div key="checkbox" className="mb-3 ">
+                      <Form.Check
+                        type="checkbox"
+                        id="defalt-checkbox"
+                        label="Recently joined"
+                      />
+                    </div>
+                  </Form>
+                </div>
+        
+                <div className="border border-secondary rounded-3 col-sm m-2">
+                  <Form>
+                    <div key="checkbox" className="mb-3">
+                      <Form.Check
+                        type="checkbox"
+                        id="defalt-checkbox"
+                        label="Recently joined"
+                      />
+                    </div>
+                  </Form>
+                  <Form>
+                    <div key="checkbox" className="mb-3 ">
+                      <Form.Check
+                        type="checkbox"
+                        id="defalt-checkbox"
+                        label="Recently joined"
+                      />
+                    </div>
+                  </Form>
+                  <Form>
+                    <div key="checkbox" className="mb-3 ">
+                      <Form.Check
+                        type="checkbox"
+                        id="defalt-checkbox"
+                        label="Recently joined"
+                      />
+                    </div>
+                  </Form>
+                </div>
+        
+                <div className="border border-secondary rounded-3 col-sm m-2">
+                  <Form>
+                    <div key="checkbox" className="mb-3">
+                      <Form.Check
+                        type="checkbox"
+                        id="defalt-checkbox"
+                        label="Recently joined"
+                      />
+                    </div>
+                  </Form>
+                  <Form>
+                    <div key="checkbox" className="mb-3 ">
+                      <Form.Check
+                        type="checkbox"
+                        id="defalt-checkbox"
+                        label="Recently joined"
+                      />
+                    </div>
+                  </Form>
+                  <Form>
+                    <div key="checkbox" className="mb-3 ">
+                      <Form.Check
+                        type="checkbox"
+                        id="defalt-checkbox"
+                        label="Recently joined"
+                      />
+                    </div>
+                  </Form>
+                </div>
+        
+              </div>
+            </div>
+          );
+        }
+        return (
+          <>
+            <OverlayTrigger
+              trigger="click"
+              key="bottom"
+              placement="bottom"
+              overlay={
+                <Popover id="popover-positioned-bottom" style={{
+                  'max-width': '600px',
+                  'width':' auto',
+                  'margin': '10px',
+                  'padding': '10px'
+                }}>
+                  <Popover.Header className="bg-light border-bottom-0" >Sort by</Popover.Header>
+                  <Popover.Body>
+                    <SortByItems/>
+                  </Popover.Body>
+                </Popover>
+              }
+            >
+              <Button variant="outline-dark">Filter</Button>
+            </OverlayTrigger>
+          </>
+        );
+      }
+
+      //Code of the searchBar with filter button
     return (
         <>
             <div className="container w-100 h-100" style={{ position: "relative" }}>
