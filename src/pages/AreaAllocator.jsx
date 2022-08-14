ada1b1620dff5d056620b9ff961ac0c9a0b4f73a
Ayushnigam1 <abhay27nigam02@gmail.com>
2022-08-15 00:29:24 +0530

    map add only

--- src/pages/AreaAllocator.jsx
+++ src/pages/AreaAllocator.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
-import { Container } from 'react-bootstrap';
+// api key=AIzaSyCxdc58Bj4jL2Q_v9IxN5fvcDbqbxOD8Ag;
 
+// install packages -- npm i @iconify/icons-mdi,@iconify/react
+// 
+import React from "react";
+import GoogleMapReact from "google-map-react";
+import { Icon } from "@iconify/react";
+import locationIcon from "@iconify/icons-mdi/map-marker";
+
+const AnyReactComponent = ({ text }) => (
+  <div className="pin">
+    <Icon icon={locationIcon} className="pin-icon" />
+    <p className="pin-text">{text}</p>
+  </div>
+);
 export default function AreaAllocator() {
+  const defaultProps = {
+    center: {
+      lat: 28.70406,
+      lng: 77.102493,
+    },
+
+    zoom: 11,
+  };
+
   return (
-    <Container className='vh-100 my-auto  mx-auto  d-flex  align-items-center justify-content-center'>
- <h1>AreaAllocator</h1>
- </Container>
-  )
+    // Important! Always set the container height explicitly
+    <div style={{ height: "100vh", width: "80%" }}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "AIzaSyCxdc58Bj4jL2Q_v9IxN5fvcDbqbxOD8Ag" }}
+        defaultCenter={defaultProps.center}
+        defaultZoom={defaultProps.zoom}
+      >
+        <AnyReactComponent lat={28.70406} lng={77.102493} text="My Marker" />
+      </GoogleMapReact>
+    </div>
+  );
 }
--- src/pages/Requests.jsx
+++ src/pages/Requests.jsx
@@ -4,33 +4,186 @@ import { Card, Col, Table, Row, Button } from "react-bootstrap";
 import { MdArrowBack } from "react-icons/md";
 
 export default function Requests() {
-  const [apiData, setApiData] = useState("");
+  const [apiData, setApiData] = useState([]);
   useEffect(() => {
-    const url = "http://127.0.0.1:8000/api/get_certificate";
-    const Data = {
-      vendorId: "62ef50d4d7038a7c8ea4c3c5",
-    };
+    const url = "http://127.0.0.1:8000/api/get_all_certificate";
+
     const fetchData = async () => {
       try {
-        const response = await fetch(url, {
-          method: "POST",
-          body: JSON.stringify(Data),
-        });
+        const response = await fetch(url);
         let tempApiData = await response.json();
         setApiData(tempApiData);
-        console.log(apiData);
       } catch (error) {
         console.log("error", error);
       }
     };
 
+    // {apiData?.map((item) => (
+    //   <ul>
+    //     <li>{item.status.label}</li>
+    //   </ul>
+    // ))}
+
     fetchData();
-  });
+  }, []);
 
   return (
-    <div
-      className="container w-100 h-100"
-      style={{ position: "relative" }}
-    ></div>
+    <div className="container w-100 h-100" style={{ position: "relative" }}>
+      {apiData.map((data) => {
+        return (
+          <Card style={{ borderBottom: "none" }}>
+            <Card.Header
+              className="d-flex justify-content-between pt-2"
+              style={{ alignItems: "center", flexDirection: "row" }}
+            >
+              <div>
+                <MdArrowBack /> <b> Details</b>
+              </div>
+              <div className="text-muted">#CertificateRrequestID</div>
+            </Card.Header>
+            <Row className="col-12">
+              <div className=" d-flex justify-content-between">
+                <div className="d-flex">
+                  <Card.Img
+                    style={{ width: "10em" }}
+                    className="p-2"
+                    variant="left"
+                    src="https://www.pngitem.com/pimgs/m/75-754636_icon-marketer-person-businessman-sales-business-business-man.png"
+                  />
+                  <div>
+                    <div>
+                      <b>Vendor Name</b>
+                    </div>
+                    <div className="text-muted d-flex ">
+                      <div> Gender</div>
+                      <div className="mx-2"> Age</div>
+                      <div className="mx-2"> {data.vendorId}</div>
+                    </div>
+                  </div>
+                </div>
+                <div style={{ width: "10em" }}>
+                  <div className="d-grid gap-2 my-3">
+                    <Button variant="primary" size="sm">
+                      Certify
+                    </Button>
+                    <Button variant="danger" size="sm">
+                      Reject
+                    </Button>
+                  </div>
+                </div>
+              </div>
+            </Row>
+
+            <Table striped bordered hover size="lg" className="w-100">
+              <thead>
+                <tr>
+                  <th>Details</th>
+                </tr>
+              </thead>
+            </Table>
+            <Row>
+              <Col className="col-6">
+                <div className=" d-flex ">
+                  <div className=" mx-5 my-1 me-5">
+                    <b>{data.document.verType}</b>
+                  </div>
+                  <div>{data.document.verId}</div>
+                </div>
+                <div className=" d-flex">
+                  <div className=" mx-5 my-1 me-5">
+                    <b>Address</b>
+                  </div>
+                  <div>
+                    <p className="p-0 m-0">
+                      some street some place some address
+                    </p>
+                    <p className="p-0 m-0">
+                      some street some place some address
+                    </p>
+                    <p className="p-0 m-0">
+                      some street some place some address
+                    </p>
+                  </div>
+                </div>
+                <div className=" mx-5 my-2">
+                  <b>Nominee Details</b>
+                </div>
+
+                <div className=" d-flex">
+                  <div className=" mx-5 my-1 me-5">
+                    <b>Nominee 1</b>
+                  </div>
+                  <div>{data.nominees.nominee1.name}</div>
+                </div>
+                <div className=" d-flex">
+                  <div className=" mx-5 my-1 me-5">
+                    <b>Relation</b>
+                  </div>
+                  <div>{data.nominees.nominee1.relation}</div>
+                </div>
+                <br></br>
+                <div className=" d-flex">
+                  <div className=" mx-5 my-1 me-5">
+                    <b>Nominee 2</b>
+                  </div>
+                  <div>{data.nominees.nominee2.name}</div>
+                </div>
+                <div className=" d-flex">
+                  <div className=" mx-5 my-1 me-5 ">
+                    <b>Relation</b>
+                  </div>
+                  <div>{data.nominees.nominee2.relation}</div>
+                </div>
+                <div className=" mx-5 my-2">
+                  <b>Business Details</b>
+                </div>
+                <div className=" d-flex">
+                  <div className=" mx-5 my-1 me-5">
+                    <b>Type</b>
+                  </div>
+                  <div>Food</div>
+                </div>
+                <div className=" d-flex">
+                  <div className=" mx-5 my-1 me-5">
+                    <b>Name</b>
+                  </div>
+                  <div>Some Name</div>
+                </div>
+              </Col>
+              <Col className="col-6">
+                <b>Photos</b>
+
+                <div className="ml-auto mr-auto d-flex">
+                  <div>
+                    {[1, 2].map((e) => (
+                      <div>
+                        <Card.Img
+                          style={{ width: "9rem" }}
+                          className="p-1"
+                          variant="left"
+                          src="https://www.pngitem.com/pimgs/m/75-754636_icon-marketer-person-businessman-sales-business-business-man.png"
+                        />
+                      </div>
+                    ))}
+                  </div>
+                  <div>
+                    {[1, 2].map((e) => (
+                      <div>
+                        <Card.Img
+                          style={{ width: "9rem" }}
+                          className="p-1"
+                          variant="left"
+                          src="https://www.pngitem.com/pimgs/m/75-754636_icon-marketer-person-businessman-sales-business-business-man.png"
+                        />
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              </Col>
+            </Row>
+          </Card>
+        );
+      })}
+    </div>
   );
 }
